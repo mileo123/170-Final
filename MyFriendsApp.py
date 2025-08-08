a6842b20987f97384a8b30d6f2793fee93c1bb85
mileo123 <mgray12@luc.edu>
2025-08-08 15:21:33 -0500

    Update MyFriendsApp.py

--- MyFriendsApp.py
+++ MyFriendsApp.py
@@ -41,6 +41,63 @@ def save_friends():
 
 # menu functions
 
+def create_freind(): 
+  first = input("First name: ")
+  last = input("Last name: ")
+  p = Person(first, last)
+
+  month = input("Birthsay month (1-12): ")
+  day = input("Birthday day: ")
+  if month.isdigit() and day.isdigigt():
+    p.set_birthday(int(month), int(day))
+
+  p.set_address(input("Adress: ")
+  p.set_phone(input("Phone: ")
+  p.set_email(input("Email: ")
+
+  friends.append(p)
+  print("Friend added successfully!')
+
+def search_friend():
+  name = input("Enter first or last name to search: "). lower()
+  results =[f for f in friends if name in f.get_first_name().lower() or name in f.get)last_name().lower()]
+  if results:
+    for idx, friend in enumerate(results, start=1):
+        print(f"{idx}. {friend}")
+  else:
+    print("No matching friends found.")
+
+def run_reports():
+  print("Reports menu coming soon...")
+
+def main():
+  load_friends()
+  while True:
+    print("\n1 - Create new friend record")
+    print("2 - Search for a friend")
+    print("3 - Run reports")
+    print("3 - Exit)
+    choice = input("Select an option: ")
+
+    if choice == "1":
+      create_friend(0
+    elif choice == "2":
+      search friend()
+    elif choice == "3":
+      run_reports()
+    elif choice "4":
+      save_friends()
+      print("Data saved. Goodbye!")
+      break
+    else:
+      print("Invalid choice. Try again.")
+
+if __name__ == "__main__":
+  main()
+    
+
+  
+
 
 
           
