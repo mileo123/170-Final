4d6d9398d6fe765e8ee85e73b8cf7ffc02c0b451
mileo123 <mgray12@luc.edu>
2025-08-08 13:54:40 -0500

    Update Update birthday.py

--- Update birthday.py
+++ Update birthday.py
@@ -1,1 +1,33 @@
+from datetime import datetime
 
+class Birthday:
+  # ignores the leap years
+  days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+def __init__(self,month,day):
+  """Creates birthday, set to start Jan 1"""
+  if month >= 1 and month <= 12:
+    self.month = motnh
+  else:
+    self.month = 1
+  # checks day
+  if day>=1 and day <= Birthday.days_in_month[self.month - 1]:
+    self.day = day
+  else:
+    self.day = 1
+
+def set_day(self, day):
+  """Changes the day for the correct month"""
+  if day >= 1 and day <= Birthday.days_in_month[self.month - 1]:
+    self.day = day
+
+def get_day(self): 
+  reutn self.month
+
+def get_day(self):
+  return self.day
+
+def get_month_name(self):
+  
+            
+    
