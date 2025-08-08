17b2251fd5a5781553c6590519f962c87e66f09f
mileo123 <mgray12@luc.edu>
2025-08-08 14:14:08 -0500

    Update UpdatedPerson.py

--- UpdatedPerson.py
+++ UpdatedPerson.py
@@ -1,77 +1,78 @@
 from Birthday import Birthday
 
 class Person:
-  def __init__(self, first_name, last_name):
-    """ A person is known by their first and last name """
-    self.first_name = first_name
-    self.last_name = last_name
-    self.birthday = None
-    self.address = ""
-    self.phone = ""
-    self.email = ""
+    def init(self, first_name, last_name):
+        """A person is known by their first and last name"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birthday = None
+        self.address = ""
+        self.phone = ""
+        self.email = ""
 
-def introduce(self):
-  """ Intro message"""
-  print("Hello, my name is", self.first_name, "and my birthday is on", self.say_birthday())
+    def introduce(self):
+        """Intro message"""
+        print("Hello, my name is", self.first_name, "and my birthday is on", self.say_birthday())
 
-def set_birthday(self, month, day):
-  """ Setting the birthday"""
-  self.birthday = Birthday(month, day)
+    def set_birthday(self, month, day):
+        """Setting the birthday"""
+        self.birthday = Birthday(month, day)
 
-def set_address(self, address):
-  self.address = address
+    def set_address(self, address):
+        self.address = address
 
-def set_phone(self, pone):
-  self.phone = phone
+    def set_phone(self, phone):
+        self.phone = phone
 
-def set_email(self.email):
-  self.email = email
+    def set_email(self, email):
+        self.email = email
 
-def get_first_name(self):
-  reutrn self.first_name
+    def get_first_name(self):
+        return self.first_name
 
-def get_last_name(self):
-  return self.last_name
+    def get_last_name(self):
+        return self.last_name
 
-def get_address(self):
-  return self.address
+    def get_address(self):
+        return self.address
 
-def get_phone(self):
-  return self.phone
+    def get_phone(self):
+        return self.phone
 
-def get_email(self):
-  return self.email
+    def get_email(self):
+        return self.email
 
-def say_birthday(self):
-  """ Returns birthday as string """
-  if self.birthday is None:
-    return "Unknown"
+    def say_birthday(self):
+        """Returns birthday as a string"""
+        if self.birthday is None:
+            return "Unknown"
 
-  day = self.birthday.get_day()
-  month_number = self.birthday.get_month()
+        day = self.birthday.get_day()
+        month_number = self.birthday.get_month()
 
-  month_names = [
-  "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
-]
-  month_name = motnh_names[month_number - 1]
+        month_names = [
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ]
+        month_name = month_names[month_number - 1]
 
-  if 11 <= day <= 13:
-  suffix = "th"
-  else:
-    last_digit = day % 10
-    if last_digit == 1:
-      suffix = "st"
-    elif last_digit == 2:
-      suffix = "nd"
-    elif last_digit == 3:
-      suffix = "rd"
-    else:
-      suffix = "th"
-  return str(day) + suffix + " of " + month_name
+        # Determine suffix
+        if 11 <= day <= 13:
+            suffix = "th"
+        else:
+            last_digit = day % 10
+            if last_digit == 1:
+                suffix = "st"
+            elif last_digit == 2:
+                suffix = "nd"
+            elif last_digit == 3:
+                suffix = "rd"
+            else:
+                suffix = "th"
 
-def __str__(self):
-  return self.first_name + " " + self.last_name
+        return str(day) + suffix + " of " + month_name
 
-
+    def str(self):
+        return self.first_name + " " + self.last_name
     
     
